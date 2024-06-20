722fb20e3fb3052e31594b8abd98822d8c92898a
gbieli <gery.bieli@gmx.ch>
2024-06-20 21:16:20 +0200

    filter dfs, combine dfs from all tables into one

--- src/expense_report/main.py
+++ src/expense_report/main.py
@@ -6,22 +6,26 @@ import tabula
 from expense_report.private import paths_pdfs
 
 
-def pdf_to_excel(pdf_file_path, excel_file_path):
+def pdfs_to_excel(pdf_files, excel_file_path, sheet_name):
     # Read PDF file
-    tables = tabula.read_pdf(pdf_file_path, pages="all")
-
+    df_list = []
+    for pdf_file_path in pdf_files:
+        dfs_from_pdf = tabula.read_pdf(pdf_file_path, pages="all")
+        if "ckverg" in str(dfs_from_pdf[-1].columns.to_list()):
+            del dfs_from_pdf[-1]
+        df_list.extend(dfs_from_pdf)
+    df_concat = pd.concat(df_list)
+    print(df_concat)
     # Write each table to a separate sheet in the Excel file
     with pd.ExcelWriter(excel_file_path) as writer:
-        for i, table in enumerate(tables):
-            table.to_excel(writer, sheet_name=f"Sheet{i+1}")
+        df_concat.to_excel(writer, sheet_name=sheet_name)
 
 
 def main():
     for account, path in paths_pdfs.items():
         p = Path(path)
         files = list(p.glob("*.pdf"))
-        for file in files:
-            pdf_to_excel(file, file.with_suffix(".xlsx"))
+        pdfs_to_excel(files, p / f"{account}_combined.xlsx", account)
 
 
 if __name__ == "__main__":
