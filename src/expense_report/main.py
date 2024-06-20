2a97750933400c43c75f724181ba755c3ce4f212
gbieli <gery.bieli@gmx.ch>
2024-06-20 17:07:21 +0200

    dev and test dependencies, refactoring

--- src/expense_report/main.py
+++ src/expense_report/main.py
@@ -1,23 +1,28 @@
-import tabula
-import pandas as pd
 from pathlib import Path
 
-from expense_report.private import paths
+import pandas as pd
+import tabula
+
+from expense_report.private import paths_pdfs
 
 
 def pdf_to_excel(pdf_file_path, excel_file_path):
     # Read PDF file
-    tables = tabula.read_pdf(pdf_file_path, pages='all')
+    tables = tabula.read_pdf(pdf_file_path, pages="all")
 
     # Write each table to a separate sheet in the Excel file
     with pd.ExcelWriter(excel_file_path) as writer:
         for i, table in enumerate(tables):
-            table.to_excel(writer, sheet_name=f'Sheet{i+1}')
+            table.to_excel(writer, sheet_name=f"Sheet{i+1}")
 
 
-if __name__ == "__main__":
-    for account, path in paths.items():
+def main():
+    for account, path in paths_pdfs.items():
         p = Path(path)
         files = list(p.glob("*.pdf"))
         for file in files:
             pdf_to_excel(file, file.with_suffix(".xlsx"))
+
+
+if __name__ == "__main__":
+    main()
